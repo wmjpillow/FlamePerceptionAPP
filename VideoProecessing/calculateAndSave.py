80e258cd1ec018e9be606dd35cb2d7bcbfbad5a4
wmjpillow <mwang3@wpi.edu>
2019-12-21 11:13:15 -0500

    save data

--- VideoProecessing/calculateAndSave.py
+++ VideoProecessing/calculateAndSave.py
@@ -1,15 +1,18 @@
 from PIL import Image
 import numpy as np
 import glob
-import csv
+import sys
+from pathlib import Path
 
 # https://stackoverflow.com/questions/57562178/calculate-the-ratio-of-area-of-different-colors-in-an-image
-
 # https://stackoverflow.com/questions/3345336/save-results-to-csv-file-with-python
 
+sys.stdout = open("data.txt", "w")
 
 def processLog(filename):
-    print(f"Processing log: {filename}")
+    # print(f"Processing log: {filename}")
+    p=Path(f"{filename}")
+    # print(f"{p.stem}")
     # Open this image and make a Numpy version for easy processing
     im   = Image.open(filename).convert('RGBA').convert('RGB')
     imnp = np.array(im)
@@ -18,31 +21,24 @@ def processLog(filename):
     # Get list of unique colours...
     # Arrange all pixels into a tall column of 3 RGB values and find unique rows (colours)
     colours, counts = np.unique(imnp.reshape(-1,3), axis=0, return_counts=1)
-
+    # Get area and portion of black color
     SumCount=0
     SumProportion=0
 
-    with open('data.csv', 'w') as csvfile:
-       csvwrit = csv.writer(csvfile)
-       # csvwrit.writerow(["SumCount", "SumProportion"])
-
     # Iterate through unique colours
     for index, colour in enumerate(colours):
-
         count = counts[index]
         proportion = (100 * count) / (h * w)
         # print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
         if index<=20:
           SumCount=SumCount+count
           SumProportion=SumProportion+proportion
-          # print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
-    print(f"   sumcount: {SumCount}")
-    print(f"   SumProportion: {SumProportion}")
-    # np.savetxt((SumCount, SumProportion), delimiter=',', fmt='%s')
-    csvwrit.writerow({SumCount}, {SumProportion})
-
-
+    # print(f"   sumcount: {SumCount}")
+    # print(f"   SumProportion: {SumProportion}")
+    print(f" {p.stem} {SumCount} {SumProportion}  ")
 
 # Iterate over all images called "log*png" in current directory
 for filename in glob.glob("/Users/wangmeijie/ALLImportantProjects/Flame+MaskRCNN/data/JPEGImages_NG/*.jpg"):
     processLog(filename)
+
+sys.stdout.close()
