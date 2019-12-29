a573c36e1a88ced6892c2504b3d3e6195912385a
wmjpillow <mwang3@wpi.edu>
2019-12-29 09:52:06 -0500

    data sort

--- VideoProecessing/visualization.py
+++ VideoProecessing/visualization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 #https://stackoverflow.com/questions/34158103/sort-a-file-by-first-or-second-or-else-column-in-python
 
@@ -8,8 +9,9 @@ df=pd.read_csv('data.csv',  names=['a','b','c'])
 
 df['a'] = df['a'].str.replace(r'\D', '')
 
+df.a = pd.to_numeric(df.a, errors='coerce')
+df = df.sort_values('a', ascending=True)
 
-df = df.sort_values('a')
 print (df)
 
 df.to_csv('/Users/wangmeijie/ALLImportantProjects/Flame+MaskRCNN/data/sortedData.csv', encoding='utf-8', index=False)
