a4e6a3f857bef725a811406a45be4c3efc11d31b
wmjpillow <mwang3@wpi.edu>
2020-01-14 16:30:09 -0500

    video processing

---
+++ VideoProecessing/visualization.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+#https://stackoverflow.com/questions/34158103/sort-a-file-by-first-or-second-or-else-column-in-python
+
+df=pd.read_csv('data.csv',  names=['a','b','c'])
+# df=df.sort_values(["frame"], axis=0)
+# df.to_csv (r'/Users/wangmeijie/ALLImportantProjects/Flame+MaskRCNN/data/sortedData.csv', index = None, header=True)
+
+df['a'] = df['a'].str.replace(r'\D', '')
+
+df.a = pd.to_numeric(df.a, errors='coerce')
+df = df.sort_values('a', ascending=True)
+
+print (df)
+
+df.to_csv('/Users/wangmeijie/ALLImportantProjects/Flame+MaskRCNN/data/sortedData.csv', encoding='utf-8', index=False)
