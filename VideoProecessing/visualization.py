27ee6e91848f1a7477d3e01295ec149ed9ee7a08
wmjpillow <mwang3@wpi.edu>
2020-01-18 08:43:53 -0500

    vis

--- VideoProecessing/visualization.py
+++ VideoProecessing/visualization.py
@@ -3,15 +3,19 @@ import numpy as np
 
 #https://stackoverflow.com/questions/34158103/sort-a-file-by-first-or-second-or-else-column-in-python
 
+# Area
+
 df=pd.read_csv('data.csv',  names=['I','S','P'])
-# df=df.sort_values(["frame"], axis=0)
-# df.to_csv (r'/Users/wangmeijie/ALLImportantProjects/Flame+MaskRCNN/data/sortedData.csv', index = None, header=True)
-
 df['I'] = df['I'].str.replace(r'\D', '')
-
 df.I = pd.to_numeric(df.I, errors='coerce')
 df = df.sort_values('I', ascending=True)
-
 print (df)
-
 df.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/sortedData.csv', encoding='utf-8', index=False)
+
+#Height
+dh=pd.read_csv('height.csv',  names=['N','H'])
+
+# df.N = pd.to_numeric(df.N, errors='coerce')
+# df = df.sort_values('I', ascending=True)
+print (dh)
+dh.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/heightData.csv', encoding='utf-8', index=False)
