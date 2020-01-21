416ece6f9ae098af3e4ea2ae647b968dfb650007
wmjpillow <mwang3@wpi.edu>
2020-01-21 04:50:27 -0500

    update

--- VideoProecessing/visualization.py
+++ VideoProecessing/visualization.py
@@ -5,17 +5,22 @@ import numpy as np
 
 # Area
 
-df=pd.read_csv('data.csv',  names=['I','S','P'])
-df['I'] = df['I'].str.replace(r'\D', '')
-df.I = pd.to_numeric(df.I, errors='coerce')
-df = df.sort_values('I', ascending=True)
-print (df)
-df.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/sortedData.csv', encoding='utf-8', index=False)
+# df=pd.read_csv('data.csv',  names=['I','S','P'])
+# df['I'] = df['I'].str.replace(r'\D', '')
+# df.I = pd.to_numeric(df.I, errors='coerce')
+# df = df.sort_values('I', ascending=True)
+# print (df)
+# df.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/sortedData.csv', encoding='utf-8', index=False)
 
 #Height
-dh=pd.read_csv('height.csv',  names=['N','H'])
+# dh=pd.read_csv('height.csv',  names=['N','H'])
+#
+# # df.N = pd.to_numeric(df.N, errors='coerce')
+# # df = df.sort_values('I', ascending=True)
+# print (dh)
+# dh.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/heightData.csv', encoding='utf-8', index=False)
 
-# df.N = pd.to_numeric(df.N, errors='coerce')
-# df = df.sort_values('I', ascending=True)
-print (dh)
-dh.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/heightData.csv', encoding='utf-8', index=False)
+
+dD=pd.read_csv('Data1.csv',  names=['I','H','S','P'])
+print (dD)
+dD.to_csv('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/data/Data1.csv', encoding='utf-8', index=False)
