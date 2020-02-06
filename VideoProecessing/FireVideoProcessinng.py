d7d29e8eb035d8addb80f4a8aa443a1f16a7d13d
wmjpillow <mwang3@wpi.edu>
2020-02-06 08:26:25 -0500

    update processing

--- VideoProecessing/FireVideoProcessinng.py
+++ VideoProecessing/FireVideoProcessinng.py
@@ -26,50 +26,50 @@ with open("Data.csv", "w", newline='') as csvFile:
 # print('I', ',', 'H', ',', 'S', ',', 'P')
     ID = 0
     while 1:
-       ret, frame = cap1.read()  # reading the image
-       if count%10==0:
-       # flame shape area
-          gs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-          gs_neg = 255 - gs.astype(int)
-          gs_neg[gs_neg > 50] = 1000
-          gs_neg = gs_neg.astype('uint8')
-          show_imagename = ['negative']
-          show_image = [gs_neg]
-          im   = Image.fromarray(gs_neg).convert('RGBA').convert('RGB')
-          imnp = np.array(im)
-          h, w = imnp.shape[:2]
-          colours, counts = np.unique(imnp.reshape(-1,3), axis=0, return_counts=1)
-          SumCount=0
-          SumProportion=0
-          for index, colour in enumerate(colours):
-             count = counts[index]
-             proportion = (100 * count) / (h * w)
-             if index<=20:
-               SumCount=SumCount+count
-               SumProportion=SumProportion+proportion
+        ret, frame = cap1.read()  # reading the image
+        if count%10==0:
+        # flame shape area
+            gs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gs_neg = 255 - gs.astype(int)
+            gs_neg[gs_neg > 50] = 1000
+            gs_neg = gs_neg.astype('uint8')
+            show_imagename = ['negative']
+            show_image = [gs_neg]
+            im   = Image.fromarray(gs_neg).convert('RGBA').convert('RGB')
+            imnp = np.array(im)
+            h, w = imnp.shape[:2]
+            colours, counts = np.unique(imnp.reshape(-1,3), axis=0, return_counts=1)
+            SumCount=0
+            SumProportion=0
+            for index, colour in enumerate(colours):
+               count = counts[index]
+               proportion = (100 * count) / (h * w)
+               if index<=20:
+                 SumCount=SumCount+count
+                 SumProportion=SumProportion+proportion
 
         # Bounding Box
-          sub_image = fgbg.apply(frame)  # background subtraction
-          ret, thresh = cv2.threshold(sub_image, 127, 255, 0)  # thresholding
-          contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # finding the contours
-          areas = [cv2.contourArea(c) for c in contours]
-          if len(areas) > 1:
-             max_index = np.argmax(areas)
-             cnt = contours[max_index]
-             if areas[max_index] > 70:
-                x, y, w, h = cv2.boundingRect(cnt)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                string_ = "fire" + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h)
-                ID = ID+1
-                print(ID, ',', str(h), ',', SumCount, ',', SumProportion)
-                cv2.putText(frame, 'fire', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                # cv2.imshow('fire detection', frame)
-          cv2.waitKey(300)
-          k = cv2.waitKey(300) & 0xFF == ord('q')
-          if k == 27:
-            break
-       count+=1
+            sub_image = fgbg.apply(frame)  # background subtraction
+            ret, thresh = cv2.threshold(sub_image, 127, 255, 0)  # thresholding
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # finding the contours
+            areas = [cv2.contourArea(c) for c in contours]
+            if len(areas) > 1:
+               max_index = np.argmax(areas)
+               cnt = contours[max_index]
+               if areas[max_index] > 70:
+                  x, y, w, h = cv2.boundingRect(cnt)
+                  cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                  string_ = "fire" + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h)
+                  ID = ID+1
+                  print(ID, ',', str(h), ',', SumCount, ',', SumProportion)
+                  cv2.putText(frame, 'fire', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                  # cv2.imshow('fire detection', frame)
+            cv2.waitKey(300)
+            k = cv2.waitKey(300) & 0xFF == ord('q')
+            if k == 27:
+                break
+        count+=1
 
-cap1.release()
-cv2.destroyAllWindows()
-sys.stdout.close()
+    cap1.release()
+    cv2.destroyAllWindows()
+    sys.stdout.close()
