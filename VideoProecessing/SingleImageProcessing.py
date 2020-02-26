a2a7c0352ab25e963683b5d1073f2960560eca05
wmjpillow <mwang3@wpi.edu>
2020-02-26 06:39:57 -0800

    model

--- VideoProecessing/SingleImageProcessing.py
+++ VideoProecessing/SingleImageProcessing.py
@@ -1,11 +1,12 @@
 import cv2
 
-img = cv2.imread('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/WebApplication/static/img/Area/frame2610.jpg')
+img = cv2.imread('/Users/wangmeijie/ALLImportantProjects/FlameDetectionAPP/MaskRCNN/JPEGImages/frame1808.jpg')
 gs = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
 gs_neg = 255 - gs.astype(int)
-gs_neg[gs_neg>50] = 1000
+# gs_neg[gs_neg>800] = 1000  02/26/2020
+gs_neg[gs_neg>800] = 1000
 gs_neg = gs_neg.astype('uint8')
 
 
